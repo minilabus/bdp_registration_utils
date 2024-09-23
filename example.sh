ce8eeca9117313e1c1716fcb5c6ce8b1b2ab7243
frheault <francois.m.rheault@usherbrooke.ca>
2024-09-23 09:05:47 -0400

    comments

--- example.sh
+++ example.sh
@@ -1,3 +1,6 @@
+# How to build the Docker/Singularity
 sudo docker build . -t "bradipho" --rm --no-cache
 sudo singularity build bradipho.sif docker-daemon://bradipho:latest
-singularity exec ~/Libraries/bradipho/bdp_registration_utils/bradipho.sif bash /bdp_registration_utils/launch_registration.sh ${PATH_TO_DATA}/ sub-01
+
+# Example command to launch
+# singularity exec ~/Libraries/bradipho/bdp_registration_utils/bradipho.sif bash /bdp_registration_utils/launch_registration.sh ${PATH_TO_DATA}/ sub-01
