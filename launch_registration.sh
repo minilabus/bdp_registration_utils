32dea219f9c7ef5cdc3d9d7147ea7823550fd30b
frheault <francois.m.rheault@usherbrooke.ca>
2024-11-18 08:42:11 -0500

    Working version IS_ONLINE

--- launch_registration.sh
+++ launch_registration.sh
@@ -3,8 +3,9 @@
 TARGET_DIR=${1}
 TMP_DIR="tmp_$((1 + $RANDOM % 100))/"
 OUT_DIR="output/"
-SCRIPT_DIR="/bdp_registration_utils/"
+SCRIPT_DIR=/bdp_registration_utils/ # To be replaced by the actual path
 SUBJ=${2}
+IS_ONLINE=true
 
 # Check if the target directory exists
 if [ ! -d ${TARGET_DIR} ]; then
@@ -19,12 +20,18 @@ if [ ! -d ${SCRIPT_DIR}/${SUBJ} ]; then
 fi
 
 echo "Prepare input data and organize by file format..."
+    if [ ${IS_ONLINE} = true ] ; then
+        ANTS_OPTS=""
+    else
+        MKDIR_OPTS="${OUT_DIR}/cloud_compare/polylines"
+    fi
+
 rm -rf ${OUT_DIR}/ ${TMP_DIR}/
 mkdir -p ${TMP_DIR}/images ${TMP_DIR}/labels_masks ${TMP_DIR}/streamlines \
     ${TMP_DIR}/meshes_point_clouds
 mkdir -p ${OUT_DIR}/native/images ${OUT_DIR}/native/labels_masks \
     ${OUT_DIR}/native/streamlines ${OUT_DIR}/native/meshes_point_clouds
-mkdir -p ${OUT_DIR}/cloud_compare/tubes ${OUT_DIR}/cloud_compare/polylines \
+mkdir -p ${OUT_DIR}/cloud_compare/tubes ${MKDIR_OPTS} \
     ${OUT_DIR}/cloud_compare/meshes_point_clouds
 
 python ${SCRIPT_DIR}/reorganize_files.py ${TARGET_DIR} ${TMP_DIR}/reorganized/ -f
@@ -69,41 +76,56 @@ if ! grep -q "All input files have compatible headers" ${TMP_DIR}/log.txt; then
     echo "Registration to MNI in case the provided data does not match our MNI template..."
     # Registration from native to MNI space 
     # Use antsRegistrationSyN.sh for improved registration
+    if [ ${IS_ONLINE} = true ] ; then
+        ANTS_OPTS="-t a"
+    else
+        ANTS_OPTS="-t s"
+    fi
+
     antsRegistrationSyNQuick.sh -d 3 -m ${REFERENCE} \
-        -f ${SCRIPT_DIR}/mni_masked.nii.gz -t s -o ${TMP_DIR}/to_mni -n 1 > ${TMP_DIR}/log.txt
+        -f ${SCRIPT_DIR}/mni_masked.nii.gz ${ANTS_OPTS} -o ${TMP_DIR}/to_mni -n 1 > ${TMP_DIR}/log.txt
 
     # Apply to every bundle (after validation of header, the files are not in
     # the reorganized folder anymore). That's why we use -f, to overwrite files
     echo "Apply transform to our MNI template..."
     echo " - To streamlines"
+    if [ ${IS_ONLINE} = true ] ; then
+        WARP_OPTS=""
+    else
+        WARP_OPTS="--in_deformation ${TMP_DIR}/to_mni1InverseWarp.nii.gz"
+    fi
     for BUNDLE in ${TMP_DIR}/reorganized/streamlines/*;
         do scil_tractogram_apply_transform.py ${BUNDLE} ${SCRIPT_DIR}/mni_masked.nii.gz \
             ${TMP_DIR}/to_mni0GenericAffine.mat ${TMP_DIR}/streamlines/$(basename ${BUNDLE}) \
-            --inverse --in_deformation ${TMP_DIR}/to_mni1InverseWarp.nii.gz \
-            --cut_invalid -f;
+            --inverse ${WARP_OPTS} --cut_invalid -f;
     done
 
     echo " - To meshes and points clouds"
     for MESH in ${TMP_DIR}/reorganized/meshes_point_clouds/*;
         do scil_surface_apply_transform.py ${MESH} \
             ${TMP_DIR}/to_mni0GenericAffine.mat ${TMP_DIR}/meshes_point_clouds/$(basename ${MESH}) \
-            --inverse --in_deformation ${TMP_DIR}/to_mni1InverseWarp.nii.gz;
+            --inverse ${WARP_OPTS};
     done
 
     echo " - To images"
+    if [ ${IS_ONLINE} = true ] ; then
+        WARP_OPTS=""
+    else
+        WARP_OPTS="-t ${TMP_DIR}/to_mni1Warp.nii.gz"
+    fi
+
     for IMAGE in ${TMP_DIR}/reorganized/images/*;
         do antsApplyTransforms -d 3 -i ${IMAGE} -r ${SCRIPT_DIR}/mni_masked.nii.gz \
             -o ${TMP_DIR}/images/$(basename ${IMAGE}) \
-            -t ${TMP_DIR}/to_mni1Warp.nii.gz \
-            -t ${TMP_DIR}/to_mni0GenericAffine.mat --interpolation Linear;
+            ${WARP_OPTS} -t ${TMP_DIR}/to_mni0GenericAffine.mat \
+            --interpolation Linear;
     done
 
     echo " - To labels and masks"
     for LABEL in ${TMP_DIR}/reorganized/labels_masks/*;
         do antsApplyTransforms -d 3 -i ${LABEL} -r ${SCRIPT_DIR}/mni_masked.nii.gz \
             -o ${TMP_DIR}/labels_masks/$(basename ${LABEL}) \
-            -t ${TMP_DIR}/to_mni1Warp.nii.gz \
-            -t ${TMP_DIR}/to_mni0GenericAffine.mat \
+            ${WARP_OPTS} -t ${TMP_DIR}/to_mni0GenericAffine.mat \
             --interpolation NearestNeighbor -u short;
     done
 else
@@ -153,12 +175,15 @@ done
 echo "Generate meshes, polylines and tubes in photogrammetry space..."
 for BUNDLE in ${OUT_DIR}/native/streamlines/*
     do EXT=$(basename "${BUNDLE}" | awk -F . '{print $NF}')
-    bdp_scale_tractography_file.py ${BUNDLE} \
+
+    if [ ${IS_ONLINE} = true ];
+        then bdp_scale_tractography_file.py ${BUNDLE} \
         ${OUT_DIR}/cloud_compare/polylines/$(basename ${BUNDLE} .${EXT}).vtk
-    bdp_generate_tubes_from_streamlines.py ${OUT_DIR}/cloud_compare/polylines/$(basename \
-        ${BUNDLE} .${EXT}).vtk 0.00025 \
+    fi
+
+    bdp_generate_tubes_from_streamlines.py ${BUNDLE} 0.00025 \
         ${OUT_DIR}/cloud_compare/tubes/$(basename ${BUNDLE} .${EXT}).ply \
-        --color 255 255 255 --tol_error 0.0005 \
+        --color 255 255 255 --tol_error 0.0005 --scaling 0.001 \
         --reference ${SCRIPT_DIR}/${SUBJ}/to_specimenWarped.nii.gz
 done
 
